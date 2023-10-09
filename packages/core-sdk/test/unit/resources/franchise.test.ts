cfed86d7b921e2adffa8b61a92024f9b07c3bcb1
Ze <ze.huang@storyprotocol.xyz>
2023-10-08 20:13:20 -0700

    Update franchise tests (#24)

--- packages/core-sdk/test/unit/resources/franchise.test.ts
+++ packages/core-sdk/test/unit/resources/franchise.test.ts
@@ -1,9 +1,12 @@
-import { expect } from "chai";
+import chai, { expect } from "chai";
 import { FranchiseClient } from "../../../src/resources/franchise";
 import { FranchiseRegistry } from "../../../src/abi/generated/FranchiseRegistry";
 import { AxiosInstance } from "axios";
 import { createMock } from "../testUtils";
 import * as sinon from "sinon";
+import chaiAsPromised from "chai-as-promised";
+
+chai.use(chaiAsPromised);
 
 describe("Test FranchiseClient", function () {
   let franchise: FranchiseClient;
@@ -40,59 +43,48 @@ describe("Test FranchiseClient", function () {
     });
 
     it("should throw error", async function () {
-      try {
-        axiosMock.get = sinon.stub().throws(new Error("http 500"));
-
-        await franchise.get({
+      axiosMock.get = sinon.stub().rejects(new Error("http 500"));
+      await expect(
+        franchise.get({
           franchiseId: "6",
-        });
-
-        expect.fail(`Function should not get here, it should throw an error `);
-      } catch (error) {}
+        }),
+      ).to.be.rejectedWith("http 500");
     });
 
     it("should throw error when the franchise id is invalid", async function () {
-      try {
-        await franchise.get({
+      await expect(
+        franchise.get({
           franchiseId: "abc",
-        });
-
-        expect.fail(`Function should not get here, it should throw an error `);
-      } catch (error) {}
+        }),
+      ).to.be.rejectedWith(`Invalid franchise id. Must be an integer. But got: abc`);
     });
   });
 
   describe("Test franchise.create", async function () {
     it("should not throw error when creating a franchise", async function () {
-      try {
-        franchiseRegistryMock.registerFranchise = sinon.stub().returns({
-          hash: "0x129f7dd802200f096221dd89d5b086e4bd3ad6eafb378a0c75e3b04fc375f997",
-        });
-
-        await franchise.create({
+      franchiseRegistryMock.registerFranchise = sinon.stub().returns({
+        hash: "0x129f7dd802200f096221dd89d5b086e4bd3ad6eafb378a0c75e3b04fc375f997",
+      });
+      await expect(
+        franchise.create({
           franchiseName: "Star War",
           franchiseSymbol: "star",
           franchiseDescription:
             "A timeless space opera franchise created by George Lucas, depicting the battle between the heroic Rebel Alliance and the evil Galactic Empire, entwined with themes of hope, destiny, and the enduring struggle between light and dark.",
-        });
-      } catch (error) {
-        expect.fail(`Function should not have thrown any error, but it threw: ${error}`);
-      }
+        }),
+      ).not.to.be.rejected;
     });
 
     it("should throw error", async function () {
-      try {
-        franchiseRegistryMock.registerFranchise = sinon.stub().throws(new Error("revert"));
-
-        await franchise.create({
+      franchiseRegistryMock.registerFranchise = sinon.stub().rejects(new Error("revert"));
+      await expect(
+        franchise.create({
           franchiseName: "Star War",
           franchiseSymbol: "star",
           franchiseDescription:
             "A timeless space opera franchise created by George Lucas, depicting the battle between the heroic Rebel Alliance and the evil Galactic Empire, entwined with themes of hope, destiny, and the enduring struggle between light and dark.",
-        });
-
-        expect.fail(`Function should not get here, it should throw an error `);
-      } catch (error) {}
+        }),
+      ).to.be.rejectedWith("revert");
     });
   });
 });
