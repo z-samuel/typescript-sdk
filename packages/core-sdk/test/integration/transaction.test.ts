9bcb9d71f0709da64bc8867b23ecfb19d53c640e
Don <100505855+DonFungible@users.noreply.github.com>
2023-10-12 15:22:31 -0400

    feat: add transactions client (#31)
    * feat: add TransactionClient class
    * feat: add transaction in StoryClient. Add integration tests
    * fix: address PR comments

---
+++ packages/core-sdk/test/integration/transaction.test.ts
@@ -0,0 +1,87 @@
+import chai, { expect } from "chai";
+import chaiAsPromised from "chai-as-promised";
+import { StoryClient, StoryConfig, Environment } from "../../src/index";
+import { ethers } from "ethers";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+chai.use(chaiAsPromised);
+
+describe("Collect client integration tests", () => {
+  let client: StoryClient;
+
+  beforeEach(function () {
+    const provider = new ethers.providers.JsonRpcProvider(process.env.RPC_PROVIDER_URL);
+    const wallet = new ethers.Wallet(process.env.WALLET_PRIVATE_KEY as string, provider);
+
+    const config: StoryConfig = {
+      environment: Environment.TEST,
+      signer: wallet,
+    };
+
+    client = new StoryClient(config);
+  });
+
+  describe("List Transactions", async function () {
+    it("should return array of transactions", async () => {
+      const response = await client.transaction.list();
+      expect(response).to.have.property("Data");
+      expect(response.Data).to.be.an("array"); // Collection[]
+
+      const transaction = response.Data[0];
+      expect(transaction).to.have.property("txId");
+      expect(transaction).to.have.property("txHash");
+      expect(transaction).to.have.property("createdAt");
+      expect(transaction).to.have.property("creatorAddress");
+      expect(transaction).to.have.property("type");
+      expect(transaction).to.have.property("resourceId");
+      expect(transaction).to.have.property("franchiseId");
+      expect(transaction.txId).to.be.a("string");
+      expect(transaction.txHash).to.be.a("string");
+      expect(transaction.createdAt).to.be.a("string");
+      expect(transaction.creatorAddress).to.be.a("string");
+      expect(transaction.type).to.be.a("string");
+      expect(transaction.resourceId).to.be.a("string");
+      expect(transaction.franchiseId).to.be.a("string");
+
+      const transaction2 = response.Data[1];
+      expect(transaction2).to.have.property("txId");
+      expect(transaction2).to.have.property("txHash");
+      expect(transaction2).to.have.property("createdAt");
+      expect(transaction2).to.have.property("creatorAddress");
+      expect(transaction2).to.have.property("type");
+      expect(transaction2).to.have.property("resourceId");
+      expect(transaction2).to.have.property("franchiseId");
+      expect(transaction2.txId).to.be.a("string");
+      expect(transaction2.txHash).to.be.a("string");
+      expect(transaction2.createdAt).to.be.a("string");
+      expect(transaction2.creatorAddress).to.be.a("string");
+      expect(transaction2.type).to.be.a("string");
+      expect(transaction2.resourceId).to.be.a("string");
+      expect(transaction2.franchiseId).to.be.a("string");
+    });
+  });
+
+  describe("Get Transaction", async function () {
+    it("should return transaction from request transaction id", async () => {
+      const response = await client.transaction.get({ txId: "1" });
+      expect(response).to.have.property("Data");
+
+      const transaction = response.Data;
+      expect(transaction).to.have.property("txId");
+      expect(transaction).to.have.property("txHash");
+      expect(transaction).to.have.property("createdAt");
+      expect(transaction).to.have.property("creatorAddress");
+      expect(transaction).to.have.property("type");
+      expect(transaction).to.have.property("resourceId");
+      expect(transaction).to.have.property("franchiseId");
+      expect(transaction.txId).to.be.a("string");
+      expect(transaction.txHash).to.be.a("string");
+      expect(transaction.createdAt).to.be.a("string");
+      expect(transaction.creatorAddress).to.be.a("string");
+      expect(transaction.type).to.be.a("string");
+      expect(transaction.resourceId).to.be.a("string");
+      expect(transaction.franchiseId).to.be.a("string");
+    });
+  });
+});
