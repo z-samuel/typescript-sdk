4e11d0c7d2b3a13ad452e09351e863ed2fa8d121
Don <100505855+DonFungible@users.noreply.github.com>
2023-10-12 15:22:31 -0400

    feat: add transactions client (#31)
    * feat: add TransactionClient class
    * feat: add transaction in StoryClient. Add integration tests
    * fix: address PR comments

--- packages/core-sdk/src/client.ts
+++ packages/core-sdk/src/client.ts
@@ -1,6 +1,7 @@
 import { StoryConfig } from "./interfaces/config";
 import { Environment } from "./enums/environment";
 import { FranchiseClient } from "./resources/franchise";
+import { TransactionClient } from "./resources/transaction";
 import axios, { AxiosInstance } from "axios";
 import { HTTP_TIMEOUT } from "./constants/http";
 import { FranchiseRegistry__factory, LicensingModule__factory } from "./abi/generated/factories";
@@ -14,6 +15,7 @@ export class StoryClient {
   private readonly config: StoryConfig;
   private readonly httpClient: AxiosInstance;
   private _franchise: FranchiseClient | null = null;
+  private _transaction: TransactionClient | null = null;
 
   /**
    * @param config - the configuration for the SDK client
@@ -48,5 +50,18 @@ export class StoryClient {
       this._franchise = new FranchiseClient(this.httpClient, franchiseRegistry, licenseModule);
     }
     return this._franchise;
+  }
+
+  /**
+   * Getter for the transaction client. The client is lazily created when
+   * this method is called.
+   *
+   * @returns the TransactionClient instance
+   */
+  public get transaction(): TransactionClient {
+    if (this._transaction === null) {
+      this._transaction = new TransactionClient(this.httpClient);
+    }
+    return this._transaction;
   }
 }
---
+++ packages/core-sdk/src/resources/transaction.ts
@@ -0,0 +1,49 @@
+import { AxiosInstance, AxiosResponse } from "axios";
+import { handleError } from "../utils/errors";
+import {
+  GetTransactionRequest,
+  GetTransactionResponse,
+  ListTransactionResponse,
+} from "../interfaces/resources/transaction";
+
+/**
+ * TransactionClient allows you to view and monitor transactions on
+ * Story Protocol.
+ */
+export class TransactionClient {
+  private readonly httpClient: AxiosInstance;
+
+  constructor(httpClient: AxiosInstance) {
+    this.httpClient = httpClient;
+  }
+
+  /**
+   * Get transaction data based on the specified transaction id.
+   *
+   * @param request - the request object for getting the transaction
+   * @returns the response object that contains the fetched transaction object
+   */
+  public async get(request: GetTransactionRequest): Promise<GetTransactionResponse> {
+    try {
+      const response = await this.httpClient.get(`/transaction/${request.txId}`);
+      return response.data as GetTransactionResponse;
+    } catch (error: unknown) {
+      handleError(error, "Failed to get transaction");
+    }
+  }
+
+  /**
+   * Get transaction data based on the specified transaction id.
+   *
+   * @param request - the request object for getting the transactions
+   * @returns the response object that contains the fetched transaction object
+   */
+  public async list(): Promise<ListTransactionResponse> {
+    try {
+      const response: AxiosResponse = await this.httpClient.get(`/transaction`);
+      return response.data as ListTransactionResponse;
+    } catch (error: unknown) {
+      handleError(error, `Failed to get transactions`);
+    }
+  }
+}
